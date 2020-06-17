576de1f5a4457dfdff30fdee3f064a7b38cb7c6d
noonzib <noonzib14@gmail.com>
2020-06-17 16:57:59 +0900

    add Dynamic Layout with data

---
+++ CSharp/DynamicLayoutWithData/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Data;
+using System.Data.OleDb;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Controls;
+
+
+namespace DynamicLayoutWithData
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dim dtEmployees as DataTable;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetData_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
