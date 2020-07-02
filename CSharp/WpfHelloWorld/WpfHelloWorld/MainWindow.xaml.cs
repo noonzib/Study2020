15013e6ec9c605274e04148389558d9b50501a02
noonzib <noonzib14@gmail.com>
2020-07-02 17:22:09 +0900

    update youtube viewer

--- CSharp/WpfHelloWorld/WpfHelloWorld/MainWindow.xaml.cs
+++ CSharp/WpfHelloWorld/WpfHelloWorld/MainWindow.xaml.cs
@@ -24,5 +24,12 @@ namespace WpfHelloWorld
         {
             InitializeComponent();
         }
+
+        private void btnClickMe_Click(object sender, RoutedEventArgs e)
+        {
+            lbResult.Items.Add(pnlMain.FindResource("strPanel").ToString());
+            lbResult.Items.Add(this.FindResource("strWindow").ToString());
+            lbResult.Items.Add(Application.Current.FindResource("strApp").ToString());
+        }
     }
 }
